cca1af0e786a7d7cb64901dc16415888e667436f
R. C. Pao <60163337+rcpao-enmotus-com@users.noreply.github.com>
2020-02-07 09:47:37 -0800

    Sign all /lib/modules/$(uname -r)/misc/*.ko
    Both VMware Workstation 14-15 and VirtualBox 6.0 kernel driver files
    end up in /lib/modules/$(uname -r)/misc/*.ko.  The number of files and
    their names change between versions, so just sign all *.ko files present.
    
    $ ls -l /lib/modules/`uname -r`/misc
    total 832
    -rw-r--r-- 1 root root 548258 Feb  7 08:48 vboxdrv.ko
    -rw-r--r-- 1 root root  14882 Feb  7 08:48 vboxnetadp.ko
    -rw-r--r-- 1 root root  38274 Feb  7 08:48 vboxnetflt.ko
    -rw-r--r-- 1 root root  34770 Feb  7 08:48 vboxpci.ko
    -rw-r--r-- 1 root root 129488 Feb  4 13:55 vmmon.ko
    -rw-r--r-- 1 root root  77200 Feb  4 13:55 vmnet.ko
    
    Warn of MSI M-Flash UEFI Utility erasing all Secure Boot keys (including
    MOK keys).
    'mokutil --import /var/lib/shim-signed/mok/MOK.der' to re-add MOK.der.

--- mok-sign-ubuntu-kernel-modules.sh
+++ mok-sign-ubuntu-kernel-modules.sh
@@ -1,28 +1,55 @@
 #!/bin/bash -x
 
 
+# UEFI (BIOS) Setup Warning:
+# MSI X470 Gaming M7 AC: Just running the M-Flash utility will Restore
+#   Defaults, erasing all UEFI Secure Boot keys (including all MOKs)!
+
+
 # https://wiki.ubuntu.com/UEFI/SecureBoot/Signing
 # shows Ubuntu 18.04.1 default location of MOK.{priv,der}
 # kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der vmmon.ko
 
 
+# https://wiki.ubuntu.com/UEFI/SecureBoot/DKMS
+# sudo mokutil --import /var/lib/shim-signed/mok/MOK.der
+#   ... will ask you for a one-time transport password which you must
+#   re-enter after reboot in the blue screen MOK enrollment program.
+#   You will never need this particular transport password again.
+
+
+# https://askubuntu.com/a/813560/329961
+# mokutil --list-enrolled
+# mokutil --export # exports MOK-####.der files to current directory
+# sudo mokutil --delete MOK-0002.der
+
+
 # VMware Workstation 14-15
 # /usr/src/linux-headers-$(uname -r)/scripts/sign-file $(modinfo -n vmmon)
 # /usr/src/linux-headers-$(uname -r)/scripts/sign-file $(modinfo -n vmnet)
-for filename in vmmon vmnet; do
-	sudo kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der  $(modinfo -n $filename)
-	echo "$filename"
-done
-sudo service vmware restart
+# for filename in vmmon vmnet; do
+# 	sudo kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der  $(modinfo -n $filename)
+# 	echo "$filename"
+# done
+# sudo service vmware restart
 
 
 # VirtualBox 6.0
 # /usr/src/linux-headers-$(uname -r)/scripts/sign-file $(modinfo -n vboxdrv)
-for filename in vboxdrv; do
-	sudo kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der  $(modinfo -n $filename)
+# for filename in vboxdrv; do
+# 	sudo kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der  $(modinfo -n $filename)
+# 	echo "$filename"
+# done
+# sudo modprobe vboxdrv
+# sudo service virtualbox restart
+
+
+for filename in /lib/modules/$(uname -r)/misc/*.ko; do
+	sudo kmodsign sha512 /var/lib/shim-signed/mok/MOK.priv /var/lib/shim-signed/mok/MOK.der  $filename
 	echo "$filename"
 done
-sudo modprobe vboxdrv
+sudo service vmware restart
+#sudo modprobe vboxdrv
 sudo service virtualbox restart
 
 
